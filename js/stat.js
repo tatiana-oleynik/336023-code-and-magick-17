9fb5071e0102a9ce755e3ee8e167be22ecb85e80
Tatiana Oleynik <kodaf@rambler.ru>
2019-06-06 18:27:00 +0300

    Исправляет ошибки

--- js/stat.js
+++ js/stat.js
@@ -3,19 +3,16 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-var results = ['2725', '4025', '1244', '1339'];
-var players = ['Вы', 'Миша', 'Анна', 'Иван'];
-var colors = ['rgba(255, 0, 0, 1)',
-              'rgb(0, 0, 255)',
-              'rgba(0, 0, 255, 0.2)',
-              'rgba(0, 0, 255, 0.4)'];
+var times = ['2725', '4025', '1244', '1339'];
+var names = ['Вы', 'Миша', 'Анна', 'Иван'];
+var colors = ['rgba(255, 0, 0, 1)', 'rgb(0, 0, 255)', 'rgba(0, 0, 255, 0.2)', 'rgba(0, 0, 255, 0.4)'];
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -27,11 +24,11 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function(ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  var maxTime = getMaxElement(results);
+  var maxTime = getMaxElement(times);
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
@@ -40,12 +37,12 @@ window.renderStatistics = function(ctx, names, times) {
 
   var coordinate = 140;
 
-  for (var i = 0; i < players.length; i++) {
+  for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], coordinate, 270);
-    ctx.fillText(results[i], coordinate, 250 + ((-150 * results[i]) / maxTime) - 10);
+    ctx.fillText(names[i], coordinate, 270);
+    ctx.fillText(times[i], coordinate, 250 + ((-150 * times[i]) / maxTime) - 10);
     ctx.fillStyle = colors[i];
-    ctx.fillRect(coordinate, 250, 40, (-150 * results[i]) / maxTime);
+    ctx.fillRect(coordinate, 250, 40, (-150 * times[i]) / maxTime);
     coordinate += 90;
   }
 };
